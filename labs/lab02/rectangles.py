8843a65ff5ee5d4bb2f0dd1210acb57997827556
Aleksandar Iliev <aleksandriliev8@gmail.com>
2024-10-31 14:21:29 +0200

    Updated tests

--- labs/lab02/rectangles.py
+++ labs/lab02/rectangles.py
@@ -68,3 +68,5 @@ expected_result_2 = [
     Rectangle(Point(x=0, y=0), Point(x=-5, y=-9), 45),
     Rectangle(Point(x=3, y=4), Point(x=7, y=7), 12),
 ]
+
+assert get_areas(starting_points_2, ending_points_2, 9) == expected_result_2
